6901b85eb9cf890dda63f7f4b431ee5f095bb63b
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-06-27 12:23:00 -0300

    Added notifications

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -57,6 +59,11 @@ class AppointmentController {
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
 
+    if (req.userId === provider_id)
+      return res
+        .status(401)
+        .json({ error: "You can't create appointments with yourself" });
+
     /**
      * Check for past dates
      */
@@ -86,6 +93,21 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,25 @@
+import User from '../models/User';
+import Notification from '../schemas/Notification';
+
+class NotificationController {
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkIsProvider)
+      return res
+        .status(401)
+        .json({ error: 'Only providers can load notifications' });
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+}
+
+export default new NotificationController();
