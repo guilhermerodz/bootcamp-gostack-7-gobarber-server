5832f73cc48c6eae5dc8d4e4f9f1662e93ea5a98
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-06-27 12:39:52 -0300

    Added appointment cancel function

--- src/routes.js
+++ src/routes.js
@@ -26,10 +26,12 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
