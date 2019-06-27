5ed811e97e1efe049c7d8ac8d3613173b74ea5ef
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-06-27 00:26:09 -0300

    Added appointment pagination to index method

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,10 +6,15 @@ import Appointment from '../models/Appointment';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+    const perPage = 20;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: perPage,
+      offset: (page - 1) * perPage,
       include: [
         {
           model: User,
